0caea308d405f01b1eae0200f6b2c0ca145694e4
ArslanEge <egearslan5@gmail.com>
2024-03-05 14:52:09 +0300

    Home Page
    Kullanıcıların sırasının bastırılması

--- lib/Services/websocket.dart
+++ lib/Services/websocket.dart
@@ -1,23 +1,25 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:footttball/Helper/helper.dart';
 import 'package:footttball/Models/teamModel.dart';
 import 'package:footttball/home.dart';
+import 'package:footttball/startPage.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:flutter/material.dart';
 
 class WebSocketManager {
   WebSocketChannel? _channel;
   StreamController? _controller;
-  bool control=true;
-  String firstMessage="";
+  bool control = true;
+  String firstMessage = "";
 
-  int index=0;
-  String type="";
+  int index = 0;
+  String type = "";
 
-  String initialType="";
-  bool playerTurn=false;
+  String initialType = "";
+  bool playerTurn = false;
 
-  Function(int index,String type)? makeMove;
+  Function(int index, String type)? makeMove;
 
   static final WebSocketManager _instance = WebSocketManager._internal();
 
@@ -29,58 +31,63 @@ class WebSocketManager {
     _controller = StreamController.broadcast();
   }
 
-  void connect(String url,BuildContext context,TeamModel teammodel,String gamemode) {
+  void connect(String url, BuildContext context, TeamModel teammodel,
+      String gamemode, String room_id) {
     _channel = WebSocketChannel.connect(Uri.parse(url));
     _channel!.stream.listen((message) {
-      if(control){
-        firstMessage=message;
-        control=false;
+      if (control) {
+        firstMessage = message;
+        control = false;
       }
-      
-      
-      if(message=="ready"){
+
+      if (message == "A player has left the room $room_id.") {
+        Helper().showInfoDialog(context, "WARNING", "USER HAS LEFT THE ROOM!");
+        close();
+
+        Future.delayed(const Duration(milliseconds: 3000), () {
+          Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => StartPage()),
+        );
+
+        });
+
+        
+
+        return;
+      }
+
+      if (message == "ready") {
         Map<String, dynamic> decoded = jsonDecode(firstMessage);
 
-        
-        teammodel.nations=List<String>.from(decoded["teammodel"]["nations"]);
-        teammodel.clubs=List<String>.from(decoded["teammodel"]["clubs"]);
-        gamemode=decoded["gamemode"];
-        
-         Navigator.push(
-                   context,
-                   MaterialPageRoute(builder: (context) => TikiTakaToeGame(teammodel:teammodel, gamemode:gamemode)),
-                );
-      }
-      else {
+        teammodel.nations = List<String>.from(decoded["teammodel"]["nations"]);
+        teammodel.clubs = List<String>.from(decoded["teammodel"]["clubs"]);
+        gamemode = decoded["gamemode"];
 
-        if(message!="X" && message!="O"){
-           Map<String,dynamic> decoded=jsonDecode(message);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  TikiTakaToeGame(teammodel: teammodel, gamemode: gamemode)),
+        );
+      } else {
+        if (message != "X" && message != "O") {
+          Map<String, dynamic> decoded = jsonDecode(message);
 
-        if(decoded.containsKey("index")){
-          index=decoded["index"];
-          type=decoded["type"];
+          if (decoded.containsKey("index")) {
+            index = decoded["index"];
+            type = decoded["type"];
 
-
-        makeMove!(index,type);
-
-        }
-
-        }
-        else {
-          initialType=message;
-          if(initialType=="X"){
-            playerTurn=true;
+            makeMove!(index, type);
           }
-          else {
-            playerTurn=false;
+        } else {
+          initialType = message;
+          if (initialType == "X") {
+            playerTurn = true;
+          } else {
+            playerTurn = false;
           }
-
         }
-
-       
-        
-
-        
       }
     });
   }
@@ -95,5 +102,4 @@ class WebSocketManager {
     _channel?.sink.close();
     _controller?.close();
   }
-  
 }
--- lib/home.dart
+++ lib/home.dart
@@ -12,185 +12,179 @@ import 'package:footttball/startPage.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
-
 class TikiTakaToeGame extends StatefulWidget {
-
   final TeamModel teammodel;
   final String gamemode;
 
-
-  TikiTakaToeGame({Key? key,required this.teammodel,required this.gamemode}) : super(key: key);
+  TikiTakaToeGame({Key? key, required this.teammodel, required this.gamemode})
+      : super(key: key);
 
   @override
   _TikiTakaToeGameState createState() => _TikiTakaToeGameState();
 }
 
-
-
 class _TikiTakaToeGameState extends State<TikiTakaToeGame> {
   late List<Player> players;
   late String currentPlayer;
   late List<String> squares;
-  List<String> urls=[];
-  int teamindex=-1;
-  int countryindex=-1;
+  List<String> urls = [];
+  int teamindex = -1;
+  int countryindex = -1;
   var teamobject = getTeamInfo();
 
   @override
   void initState() {
     super.initState();
-    WebSocketManager().makeMove=makeMove;
-     getLogoUrl();
+    WebSocketManager().makeMove = makeMove;
+    getLogoUrl();
     resetGame();
-
-    
   }
 
-  void getLogoUrl()async{
-    for(int i=0; i<3; i++){
-      var value=await ApiService.getLogo(gamemode: widget.gamemode, countryname:widget.teammodel.clubs[i]);
+  void getLogoUrl() async {
+    for (int i = 0; i < 3; i++) {
+      var value = await ApiService.getLogo(
+          gamemode: widget.gamemode, countryname: widget.teammodel.clubs[i]);
       urls.add(value);
     }
-    setState(() {
-      
-    });
-    
+    setState(() {});
   }
 
-
- void fetchAndShowGridInformation(String leagueId) async {
-  // try {
-  //   final leagueInfo = await apiService.getLeagueInfo(leagueId);
-  //   final players = await apiService.getPlayersByLeague(leagueInfo);
-  //   setState(() {
-  //     this.players = players.map((playerData) => Player(
-  //       name: playerData['name'],
-  //       nationality: playerData['nationality'],
-  //       club: playerData['club'],
-  //     )).toList();
-  //   });
-  // } catch (e) {
-  //   print('Error fetching and showing grid information: $e');
-  // }
-}
-int controlTeamIndex(){
-  if(teamindex>=2){
-
-    teamindex=0;
+  void fetchAndShowGridInformation(String leagueId) async {
+    // try {
+    //   final leagueInfo = await apiService.getLeagueInfo(leagueId);
+    //   final players = await apiService.getPlayersByLeague(leagueInfo);
+    //   setState(() {
+    //     this.players = players.map((playerData) => Player(
+    //       name: playerData['name'],
+    //       nationality: playerData['nationality'],
+    //       club: playerData['club'],
+    //     )).toList();
+    //   });
+    // } catch (e) {
+    //   print('Error fetching and showing grid information: $e');
+    // }
   }
-  else {
-    teamindex++;
+  int controlTeamIndex() {
+    if (teamindex >= 2) {
+      teamindex = 0;
+    } else {
+      teamindex++;
+    }
+    return teamindex;
   }
-  return teamindex;
-}
-int controlCountryIndex(){
-  if(teamindex>=2){
 
-    teamindex=0;
+  int controlCountryIndex() {
+    if (teamindex >= 2) {
+      teamindex = 0;
+    } else {
+      teamindex++;
+    }
+    return teamindex;
   }
-  else {
-    teamindex++;
-  }
-  return teamindex;
-}
-
-
 
   void resetGame() {
     setState(() {
       squares = List.filled(16, '');
       currentPlayer = WebSocketManager().initialType;
-      print("MY TYPE IS "+currentPlayer);
+      print("MY TYPE IS " + currentPlayer);
     });
-    squares[1]="image";
-    squares[2]="image";
-    squares[3]="image";
+    squares[1] = "image";
+    squares[2] = "image";
+    squares[3] = "image";
 
     // for(int i=0; i<squares.length; i++){
     //   print(squares[i]);
     // }
   }
 
-  void makeMove(int index,String type) {
+  void makeMove(int index, String type) {
     int row = index ~/ 4;
     int col = index % 4;
 
     // Sadece son satır ve son sütun harici hücrelere izin ver
-    if (index!=-1 && squares[index] == '' && row != 0 && col != 0) {
-        squares[index] = type;
-        // currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    if (index != -1 && squares[index] == '' && row != 0 && col != 0) {
+      squares[index] = type;
+      // currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
 
-        // Kazanan kontrolü burada yapılıyor
-        // if (checkWin('X')) {
-        //   showDialog(
-        //     context: context,
-        //     builder: (_) => gameOverDialog('X'),
-        //   );
-        // } else if (checkWin('O')) {
-        //   showDialog(
-        //     context: context,
-        //     builder: (_) => gameOverDialog('O'),
-        //   );
-        // } else if (isBoardFull()) {
-        //   showDialog(
-        //     context: context,
-        //     builder: (_) => gameOverDialog('T'),
-        //   );
-        // }
-      
+      // Kazanan kontrolü burada yapılıyor
+      // if (checkWin('X')) {
+      //   showDialog(
+      //     context: context,
+      //     builder: (_) => gameOverDialog('X'),
+      //   );
+      // } else if (checkWin('O')) {
+      //   showDialog(
+      //     context: context,
+      //     builder: (_) => gameOverDialog('O'),
+      //   );
+      // } else if (isBoardFull()) {
+      //   showDialog(
+      //     context: context,
+      //     builder: (_) => gameOverDialog('T'),
+      //   );
+      // }
     }
-      var player1=checkWin("X");
-      var player2=checkWin("O");
+    var player1 = checkWin("X");
+    var player2 = checkWin("O");
 
-      if(player1 || player2){
-        setState(() {
-          WebSocketManager().playerTurn==true;
-        });
-
-      }
-                    
-      showStatus(player1,"X");
-      showStatus(player2,"O");
-
-      
-      if(!player1 && !player2) {
-        setState(() {
-        WebSocketManager().playerTurn=!WebSocketManager().playerTurn;
+    if (player1 || player2) {
+      setState(() {
+        WebSocketManager().playerTurn == true;
       });
-      }
-    
-      
-    
+    }
+
+    showStatus(player1, "X");
+    showStatus(player2, "O");
+
+    if (!player1 && !player2) {
+      setState(() {
+        WebSocketManager().playerTurn = !WebSocketManager().playerTurn;
+      });
+    }
   }
 
   bool checkWin(String player) {
-  // Horizontal win conditions
-  bool horizontalWin = (squares[5] == player && squares[6] == player && squares[7] == player) ||
-                       (squares[9] == player && squares[10] == player && squares[11] == player) ||
-                       (squares[13] == player && squares[14] == player && squares[15] == player);
-  
-  // Vertical win conditions
-  bool verticalWin = (squares[5] == player && squares[9] == player && squares[13] == player) ||
-                     (squares[6] == player && squares[10] == player && squares[14] == player) ||
-                     (squares[7] == player && squares[11] == player && squares[15] == player);
-  
-  // Diagonal win conditions
-  bool diagonalWin = (squares[5] == player && squares[10] == player && squares[15] == player) ||
-                     (squares[7] == player && squares[10] == player && squares[13] == player);
-  
-  return horizontalWin || verticalWin || diagonalWin;
-}
+    // Horizontal win conditions
+    bool horizontalWin = (squares[5] == player &&
+            squares[6] == player &&
+            squares[7] == player) ||
+        (squares[9] == player &&
+            squares[10] == player &&
+            squares[11] == player) ||
+        (squares[13] == player &&
+            squares[14] == player &&
+            squares[15] == player);
+
+    // Vertical win conditions
+    bool verticalWin = (squares[5] == player &&
+            squares[9] == player &&
+            squares[13] == player) ||
+        (squares[6] == player &&
+            squares[10] == player &&
+            squares[14] == player) ||
+        (squares[7] == player &&
+            squares[11] == player &&
+            squares[15] == player);
+
+    // Diagonal win conditions
+    bool diagonalWin = (squares[5] == player &&
+            squares[10] == player &&
+            squares[15] == player) ||
+        (squares[7] == player &&
+            squares[10] == player &&
+            squares[13] == player);
+
+    return horizontalWin || verticalWin || diagonalWin;
+  }
 
   bool isBoardFull() {
     return !squares.contains('');
   }
-  void showStatus(bool player1,String value){
 
-    if(player1){
+  void showStatus(bool player1, String value) {
+    if (player1) {
       Helper().showInfoDialog(context, "WIN!", "$value WON THE GAME ");
     }
-
-
   }
 
   @override
@@ -222,30 +216,30 @@ int controlCountryIndex(){
                   return IgnorePointer(
                     ignoring: !WebSocketManager().playerTurn,
                     child: GestureDetector(
-                      
-                      onTap: () async{
-                        var result=await Helper().showPlayerName(context, widget.teammodel.clubs[(index%4)-1], widget.teammodel.nations[(index~/4)-1]);
-                        print(result);
-                        if(result){
-                          WebSocketManager().send(jsonEncode({"index":index,"type":currentPlayer}));
-                          
-                    
+                      onTap: () async {
+
+                        if(squares[index]==""){
+                          var result = await Helper().showPlayerName(
+                            context,
+                            widget.teammodel.clubs[(index % 4) - 1],
+                            widget.teammodel.nations[(index ~/ 4) - 1]);
+                            print(result);
+                        if (result) {
+                          WebSocketManager().send(jsonEncode(
+                              {"index": index, "type": currentPlayer}));
+
                           // makeMove(index);
-                          
-                    
-                        }
-                        else {
-                          WebSocketManager().send(jsonEncode({"index":-1,"type":currentPlayer}));
+                        } else {
+                          WebSocketManager().send(
+                              jsonEncode({"index": -1, "type": currentPlayer}));
                           // currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-                    
                         }
-                    
-                        // makeMove(index);
-                    
-                      
-                    
-                    
+
+                        }
                         
+                        
+                        // makeMove(index);
+
                         // if (!checkWin('X') && !checkWin('O') && !isBoardFull()) {
                         //   makeMove(index);
                         //   if (checkWin('X')) {
@@ -274,33 +268,38 @@ int controlCountryIndex(){
                             border: Border.all(color: Colors.white),
                           ),
                           child: Center(
-                            
-                            child: (index==1 || index==2 || index==3) ?
-                            urls.isEmpty ? CircularProgressIndicator(): Image.network(urls[controlTeamIndex()])
-                            :(index==4 || index==8 || index==12) ?
-                            Image.network("https://flagsapi.com/"+teamobject.getCountry(widget.teammodel.nations[controlCountryIndex()])+"/flat/64.png")
-                            :
-                            Text(
-                              squares[index],
-                              style:TextStyle(fontSize: 20,fontWeight:FontWeight.bold),
-                            )
-                    //         squares[index].length > 1  ? FutureBuilder<String>(
-                    //   future: ApiService.getLogo(gamemode: widget.gamemode, countryname: widget.teammodel.clubs[controlTeamIndex()]), // Assuming this function fetches the image URL from an API
-                    //   builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                    //     if (snapshot.connectionState == ConnectionState.waiting) {
-                    //       return CircularProgressIndicator(); // Show loading spinner while waiting for data
-                    //     } else if (snapshot.hasError) {
-                    //       return Icon(Icons.error); // Show error icon in case of an error
-                    //     } else {
-                    //       return Image.network(snapshot.data!); // Display the image from the URL
-                    //     }
-                    //   },
-                    // )
-                    //         :Text(
-                    //           squares[index],
-                    //           style: TextStyle(fontSize: 15),
-                    //         ),
-                          ),
+                              child: (index == 1 || index == 2 || index == 3)
+                                  ? urls.isEmpty
+                                      ? CircularProgressIndicator()
+                                      : Image.network(urls[controlTeamIndex()])
+                                  : (index == 4 || index == 8 || index == 12)
+                                      ? Image.network("https://flagsapi.com/" +
+                                          teamobject.getCountry(widget.teammodel
+                                              .nations[controlCountryIndex()]) +
+                                          "/flat/64.png")
+                                      : Text(
+                                          squares[index],
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                              //         squares[index].length > 1  ? FutureBuilder<String>(
+                              //   future: ApiService.getLogo(gamemode: widget.gamemode, countryname: widget.teammodel.clubs[controlTeamIndex()]), // Assuming this function fetches the image URL from an API
+                              //   builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                              //     if (snapshot.connectionState == ConnectionState.waiting) {
+                              //       return CircularProgressIndicator(); // Show loading spinner while waiting for data
+                              //     } else if (snapshot.hasError) {
+                              //       return Icon(Icons.error); // Show error icon in case of an error
+                              //     } else {
+                              //       return Image.network(snapshot.data!); // Display the image from the URL
+                              //     }
+                              //   },
+                              // )
+                              //         :Text(
+                              //           squares[index],
+                              //           style: TextStyle(fontSize: 15),
+                              //         ),
+                              ),
                         ),
                       ),
                     ),
@@ -310,6 +309,24 @@ int controlCountryIndex(){
               ),
             ),
           ),
+          Positioned(
+              left: screenSize.width * 0.40,
+              bottom: screenSize.height * 0.25,
+              child: Column(
+                children: [
+                  Text("TURN",
+                      style: TextStyle(color: Colors.white, fontSize: 30)),
+                  Container(
+                    width: 35.0, // Specify the width of the circle
+                    height: 35.0, // Specify the height of the circle
+                    decoration: BoxDecoration(
+                      color:WebSocketManager().playerTurn ? Colors.green :Colors.red, // Define the color of the circle
+                      shape: BoxShape
+                          .circle, // Define the shape of the container as a circle
+                    ),
+                  )
+                ],
+              )),
           Positioned(
             left: screenSize.width * 0.55,
             bottom: screenSize.height * 0.05,
@@ -333,14 +350,10 @@ int controlCountryIndex(){
             bottom: screenSize.height * 0.05,
             child: GestureDetector(
               onTap: () async {
-                
-
                 // var object= new ApiService("http://localhost:8000");
                 // var value=await object.getFinalGrid("TR1");
 
-
                 resetGame();
-                
               },
               child: Image.asset(
                 'images/replay.png',
@@ -350,7 +363,6 @@ int controlCountryIndex(){
               ),
             ),
           ),
-          
         ],
       ),
     );
@@ -371,7 +383,7 @@ int controlCountryIndex(){
         TextButton(
           onPressed: () {
             resetGame();
-            
+
             Navigator.of(context).pop();
           },
           child: Text('Yeniden Başla'),
